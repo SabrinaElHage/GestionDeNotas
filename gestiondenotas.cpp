37305fe5204d16d9ee8991979d16e01ebb64608b
SabrinaElHage <sabrinaelhagegagliardi@gmail.com>
2026-03-24 12:04:16 -0400

    agg mas funciones

--- gestiondenotas.cpp
+++ gestiondenotas.cpp
@@ -36,6 +36,9 @@ config miconfig;
 
 int leerEnteros(string mensaje);
 float leerDecimal(string mensaje);
+void gotoxi(int x, int y);
+void color(int tex, int fon);
+void dibujarFondo();
 
 //funcion principal
 int main(){
@@ -47,6 +50,8 @@ int main(){
 }
 
 //desarrollo de funciones
+
+//enteros
 int leerEnteros(string mensaje){
 
     int numero;
@@ -63,4 +68,52 @@ int leerEnteros(string mensaje){
             return numero;
         }
     }
+}//fin de la funcion
+
+//decimales
+float leerDecimal(string mensaje){
+
+    float numero;
+
+    while(true){
+        cout << mensaje;
+        cin >> numero;
+
+        if(cin.fail()){
+            cout << "Entrada no valida. Por favor, ingrese un numero decimal." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            return numero;
+        }
+    }
+}//fin de la funcion
+
+void gotoxi(int x, int y){
+
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}//fin de la funcion
+
+//colores
+void color(int tex, int fon){
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (tex + (fon * 16)));
+
+}//fin de la funcion
+
+//fondo
+void dibujarFondo(){
+
+    for(int i = 0; i < 25; i++){
+        for(int j = 0; j < 80; j++){
+            gotoxi(j, i);
+            color(miconfig.colorTexto, miconfig.colorFondo);
+            cout << " ";
+        }
+    }
+
 }//fin de la funcion
