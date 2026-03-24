3e4276d503367812117e754f970f7fd8614aafc8
SabrinaElHage <sabrinaelhagegagliardi@gmail.com>
2026-03-24 11:36:09 -0400

    otra parte

--- gestiondenotas.cpp
+++ gestiondenotas.cpp
@@ -1,18 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <limits>
+#include <iomanip>
+
+#define MAX_MATERIAS 15
+#define MAX_USUARIOS 2
 
 using namespace std;
 
 //estructura
+struct config{
 
-//come el doble de verga
+    int colorTexto = 15;
+    int colorFondo = 1;
+    int colorMenu = 14;
+
+};
+
+struct estudiante{
+
+    string nombre;
+    float notas[MAX_MATERIAS];
+
+};
+
+//declaraciones
+vector<string> listaMaterias;
+config miconfig;
+
+//funciones
+
+int leerEnteros(string mensaje);
 
 //funcion principal
 int main(){
 
-    cout<<"hola";
+    
 
 
     return 0;
 }
 
 //desarrollo de funciones
+int leerEnteros(string mensaje){
+
+    int numero;
+
+    while(true){
+        cout << mensaje;
+        cin >> numero;
+
+        if(cin.fail()){
+            cout << "Entrada no valida. Por favor, ingrese un numero entero." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            return numero;
+        }
+    }
+}
